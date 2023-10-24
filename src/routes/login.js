c81d836cf03ff461e8d14dbe52b7997a183476e2
Vinicius Alves <vinicius.s.alves26@gmail.com>
2023-10-24 16:24:09 -0300

    adicionada a rota de login validação pelo token

--- src/routes/login.js
+++ src/routes/login.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const router = express();
+require('dotenv').config();
 
 router.use(express.json());
 
 router.post("/", (req, res) => {
     const { registro, senha } = req.body;
+    const token = req.headers.authorization;
+    token === process.env.TOKEN ? isValidToken = true : isValidToken = false;
 
     res.header("Access-Control-Allow-Origin", "*");
-    if(registro && senha){
-        console.log('enviando post');
+    if(isValidToken){
         res.status(200).send({ 
             token: "Y9G4q^$@ws8!okusX$&xrn!4usEHt5Uw@qDs5Cc8v$2ze57HCV8@d#MfZ%7%6&6x",
             message: "Login efetuado com sucesso",
