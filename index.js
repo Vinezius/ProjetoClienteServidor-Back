9d0f7c916160c6b71d1b071f3aaab96842689f10
Vinicius Alves <vinicius.s.alves26@gmail.com>
2023-11-08 14:29:40 -0300

    prompt para o usuario para escolher a porta

--- index.js
+++ index.js
@@ -4,10 +4,23 @@ const server = express();
 const login = require('./src/routes/login');
 const logout = require('./src/routes/logout');
 const usuarios = require('./src/routes/usuarios');
+const prompt = require('prompt');
 
-server.use(cors());
-server.use("/login", login);
-server.use("/logout", logout);
-server.use("/usuarios", usuarios);
-server.use("/usuarios/:registro", usuarios);
-server.listen(21000, () => console.log('Ouvindo na porta 21000'));
+prompt.start();
+
+prompt.get(['portaConexao'], function (err, result) {
+    if (err) { return onErr(err); }
+    const portaConexao = result.portaConexao;
+
+    server.use(cors());
+    server.use("/login", login);
+    server.use("/logout", logout);
+    server.use("/usuarios", usuarios);
+    server.use("/usuarios/:registro", usuarios);
+    server.listen(portaConexao, () => console.log(`Ouvindo na porta ${portaConexao}`));
+});
+
+function onErr(err) {
+    console.log(err);
+    return 1;
+}
